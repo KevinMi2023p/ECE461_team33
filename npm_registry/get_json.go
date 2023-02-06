f55e4b90acad9b40ff923db08aeaa7ea67b070ff
Jaime Laster <jamie.laster23@gmail.com>
2023-02-05 21:48:22 -0500

    Simplified reading values from json and added testing

--- npm_registry/get_json.go
+++ npm_registry/get_json.go
@@ -9,7 +9,12 @@ import (
 
 const npm_registry_url_part string = "https://registry.npmjs.org/%s"
 
-func npm_registry_get_json(pkg string) map[string]interface{} {
+type npm_info struct {
+	hasRepo bool
+	repoUrl string
+}
+
+func get_json(pkg string) map[string]interface{} {
 	res, gerr := http.Get(fmt.Sprintf(npm_registry_url_part, pkg))
 
 	if (gerr != nil) {
@@ -30,4 +35,52 @@ func npm_registry_get_json(pkg string) map[string]interface{} {
 	}
 
 	return data
+}
+
+func get_value_from_map(i map[string]interface{}, key string) interface{} {
+	value, ok := i[key]
+	if (ok) {
+		return value
+	} else {
+		return nil
+	}
+}
+
+func set_repo_from_json(info *npm_info, data map[string]interface{}) {
+	info.hasRepo = false
+	info.repoUrl = ""
+
+	repoValue := get_value_from_map(data, "repository")
+
+	if (repoValue != nil) {
+		repoType := get_value_from_map(repoValue.(map[string]interface{}), "type")
+
+		if (repoType != nil && repoType.(string) == "git") {
+			repoString := get_value_from_map(repoValue.(map[string]interface{}), "url")
+
+			if (repoString != nil) {
+				url := repoString.(string)
+
+				if (len(url) > 4) {
+					info.hasRepo = true
+					info.repoUrl = url[4:]
+					return
+				}
+			}
+		}
+	}
+}
+
+func get_info(pkg string) *npm_info {
+	data := get_json(pkg)
+
+	if (data == nil) {
+		return nil
+	}
+
+	var info *npm_info = new(npm_info)
+
+	set_repo_from_json(info, data)
+
+	return info
 }
--- npm_registry/main.go
+++
@@ -1,5 +0,0 @@
-package npm_registry
-
-func main() {
-	npm_registry_get_json("xml2js")
-}
