cfc71f88a06ff4cde58ce87b8458261d726caecf
Jaime Laster <jamie.laster23@gmail.com>
2023-02-11 16:19:58 -0500

    updated imports so this can be used as a template

--- bus_factor/bf.go
+++ bus_factor/bf.go
@@ -1,11 +1,11 @@
 package bus_factor
 
 import (
-	"github.com/KevinMi2023p/ECE461_TEAM33/npm_registry"
+	"github.com/KevinMi2023p/ECE461_TEAM33/npm"
 )
 
-// alias the type because I don't feel like typing npm_registry.NpmInfo more than 2 times
-type NpmInfo = npm_registry.NpmInfo
+// alias the type because I'm lazy
+type NpmInfo = npm.NpmInfo
 
 func calculate_bus_factor(info *NpmInfo) float32 {
 	return 0.0
