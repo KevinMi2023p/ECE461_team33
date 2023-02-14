e0633335bf154b6c6dd74509e5d87b158bf0ade3
Jaime Laster <jamie.laster23@gmail.com>
2023-02-14 16:04:03 -0500

    Fixed naming convention

--- license_compatibility/license_compatibility.go
+++ license_compatibility/license_compatibility.go
@@ -7,7 +7,7 @@ import (
 	"github.com/KevinMi2023p/ECE461_TEAM33/npm"
 )
 
-func getLicenseScore(json_data *npm.NpmInfo) float32 {
+func License_compatibity(json_data *npm.NpmInfo) float32 {
 	if json_data == nil {
 		fmt.Println("NPM package doesn't exist or hasn't been called")
 		return 0
@@ -25,7 +25,6 @@ func getLicenseScore(json_data *npm.NpmInfo) float32 {
 
 // Checks the output of getLicenseScore and assigns score of 0 if no output and or 1.0 is license is read
 func calculate_license_score(github_readme_str string) float32 {
-	
 	project_licenses := []string{"afl-3.0", "apache-2.0", "artistic-2.0", "bsl-1.0", "bsd-2-clause",
 	"bsd-3-clause", "bsd-3-clause-clear", "cc", "cc0-1.0", "cc-by-4.0", "cc-by-sa-4.0", "wtfpl", 
 	"ecl-2.0", "epl-1.0", "epl-2.0", "eupl-1.1", "agpl-3.0", "gpl", "gpl-2.0", "gpl-3.0", "lgpl", 
