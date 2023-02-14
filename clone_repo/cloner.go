cf78459edc67c181e4ad248a2af2816f49f0dc1f
iwus <iwus@purdue.edu>
2023-02-14 15:50:10 -0500

    clone rep tester fix

--- clone_repo/cloner.go
+++ clone_repo/cloner.go
@@ -1,15 +1,15 @@
-package cloner
+package clone_repo
 
 import (
-    "os"
-    "os/exec"
+	"os"
+	"os/exec"
 )
 
-func cloneRepo(url string, dir string) error {
-    // Clone the repository using Git
-    cmd := exec.Command("git", "clone", url, dir)
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    err := cmd.Run()
-    return err
+func CloneRepo(url string, dir string) error {
+	// Clone the repository using Git
+	cmd := exec.Command("git", "clone", url, dir)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	return err
 }
--- clone_repo/cloner_test.go
+++ clone_repo/cloner_test.go
@@ -1,33 +1,35 @@
-package cloner
+package clone_repo
+
 import (
-	"os"
-	"testing"
 	"fmt"
-    "path/filepath"
+	"os"
+	"path/filepath"
+	"testing"
 )
-//"os/exec"
+
+// "os/exec"
 func TestMain(m *testing.M) {
-	
-    // Create the "results" directory in the current working directory
-    dir := "results"
-    err := os.Mkdir(dir, 0755)
-    if err != nil {
-        fmt.Println("Error creating directory:", err)
-        return
-    }
 
-    // Get the absolute path to the "results" directory
-    absPath, err := filepath.Abs(dir)
-    if err != nil {
-        fmt.Println("Error getting absolute path:", err)
-        return
-    }
+	// Create the "results" directory in the current working directory
+	dir := "results"
+	err := os.Mkdir(dir, 0755)
+	if err != nil {
+		fmt.Println("Error creating directory:", err)
+		return
+	}
 
-    // Call the cloneRepo function with the repository URL and "results" directory
-    err = cloneRepo("https://github.com/KevinMi2023p/ECE461_team33", absPath)
-    if err != nil {
-        fmt.Println("Error cloning repository:", err)
-    } else {
-        fmt.Println("Repository cloned successfully!")
-    }
+	// Get the absolute path to the "results" directory
+	absPath, err := filepath.Abs(dir)
+	if err != nil {
+		fmt.Println("Error getting absolute path:", err)
+		return
+	}
+
+	// Call the cloneRepo function with the repository URL and "results" directory
+	err = CloneRepo("https://github.com/KevinMi2023p/ECE461_team33", absPath)
+	if err != nil {
+		fmt.Println("Error cloning repository:", err)
+	} else {
+		fmt.Println("Repository cloned successfully!")
+	}
 }
