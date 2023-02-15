3643b39cc296c254f15b6edb76399a3db123bf51
Jaime Laster <jamie.laster23@gmail.com>
2023-02-14 22:57:22 -0500

    Cloner test needs to not permanently modify the contents of the directory

--- clone_repo/cloner_test.go
+++ clone_repo/cloner_test.go
@@ -11,15 +11,15 @@ import (
 func TestMain(m *testing.M) {
 
 	// Create the "results" directory in the current working directory
-	dir := "results"
-	err := os.Mkdir(dir, 0755)
+	dir := "../temp"
+	err := os.MkdirAll(dir + "/ECE461_team33", 0755)
 	if err != nil {
 		fmt.Println("Error creating directory:", err)
 		return
 	}
 
 	// Get the absolute path to the "results" directory
-	absPath, err := filepath.Abs(dir)
+	absPath, err := filepath.Abs(dir + "/ECE461_team33")
 	if err != nil {
 		fmt.Println("Error getting absolute path:", err)
 		return
@@ -32,4 +32,7 @@ func TestMain(m *testing.M) {
 	} else {
 		fmt.Println("Repository cloned successfully!")
 	}
+
+	// delete the directory
+	err = os.RemoveAll(dir)
 }
