761627821a421a6c32e2cb144f742890e8b1e7bd
KevinMi2023p <70293835+KevinMi2023p@users.noreply.github.com>
2023-02-14 22:11:35 -0500

    moving some code, delete duplicate

--- rampuptime/ramp_up_time_test.go
+++ rampuptime/ramp_up_time_test.go
@@ -9,12 +9,12 @@ import (
 
 func TestMain(m *testing.M) {
 	pkgs := []string{
-		"xml2js",
-		"bluebird",
-		"aws-sdk",
-		"ts-node",
-		"tar",
-		"fake",
+		"https://registry.npmjs.org/xml2js",
+		"https://registry.npmjs.org/bluebird",
+		"https://registry.npmjs.org/aws-sdk",
+		"https://registry.npmjs.org/ts-node",
+		"https://registry.npmjs.org/tar",
+		"https://registry.npmjs.org/fake",
 	}
 
 	for _, pkg := range pkgs {
@@ -24,7 +24,7 @@ func TestMain(m *testing.M) {
 		if info == nil {
 			fmt.Println("Couldn't get npm registry")
 		} else {
-			fmt.Println(Ramp_up_score(info))
+			fmt.Println(Ramp_up_score_npm(info))
 			// fmt.Println((*info)["readme"])
 		}
 	}
