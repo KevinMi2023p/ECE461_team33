0eb93773a093711bb784eadb6d5367f6f3ee00e3
Jaime Laster <jamie.laster23@gmail.com>
2023-02-14 23:20:49 -0500

    The cloner needed to learn to be quiet

--- clone_repo/cloner.go
+++ clone_repo/cloner.go
@@ -1,15 +1,14 @@
 package clone_repo
 
 import (
-	"os"
 	"os/exec"
 )
 
 func CloneRepo(url string, dir string) error {
 	// Clone the repository using Git
 	cmd := exec.Command("git", "clone", url, dir)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	cmd.Stdout = nil
+	cmd.Stderr = nil
 	err := cmd.Run()
 	//if command returns and error then this function returns and error
 	return err
